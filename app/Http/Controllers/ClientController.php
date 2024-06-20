5b3db4e7a143182d40565a21ed0d2ea9057fc20f
Alvaro Antony Villanueva Barreto <barretoalvaro369@gmail.com>
2024-06-20 17:03:49 -0500

    Listar todos los clientes, listar un cliente, mostrar errores en el Log y crear las rutas para los endpints

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,13 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ClientController extends Controller
 {
-    
+
     public function index()
     {
-        
+        $clients = Client::all();
+
+        if ($clients->isEmpty()) {
+            $data = [
+                'status' => 200,
+                'message' => 'No se encontró clientes',
+            ];
+            return response()->json($data, 200);
+        }
+
+        $data = [
+            'status' => 200,
+            'data' => $clients,
+        ];
+
+        return response()->json($data, 200);
     }
 
     /**
@@ -24,15 +40,36 @@ class ClientController extends Controller
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Client $client)
+
+    public function show($id)
     {
-        //
+        try {
+            // Intenta encontrar al cliente por su ID
+            $client = Client::find($id);
+            Log::debug('Client found: ', ['client' => $client]);
+
+            // Verifica si el cliente no fue encontrado
+            if (!$client) {
+                $data = [
+                    'status' => 404,
+                    'message' => 'No se encontró al cliente'
+                ];
+                return response()->json($data, 404);
+            }
+
+            // Retorna el cliente encontrado
+            return response()->json($client, 200);
+        } catch (\Exception $e) {
+            // Registra el error en el log
+            Log::error('Error al obtener el cliente: ', ['error' => $e->getMessage()]);
+
+            // Retorna una respuesta JSON con el error
+            $data = [
+                'status' => 500,
+                'message' => 'Ocurrió un error al obtener el cliente'
+            ];
+            return response()->json($data, 500);
+        }
     }
 
     /**
